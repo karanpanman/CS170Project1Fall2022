dfa992b2ddfc33bfdb8cbb0bcfb152a4f2b27f5b
Karan Bhogal <kbhog001@ucr.edu>
2022-10-18 11:31:09 -0700

    Initial Commit

---
+++ CS170Project1Fall2022/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  CS170Project1Fall2022
+//
+//  Created by Karan Bhogal on 10/18/22.
+//
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+        cout << "Hello, World!\n";
+    
+    return 0;
+}
